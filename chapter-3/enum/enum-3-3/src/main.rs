314e74220982b2878ee55d2cbd46830febd090e5
flyflypeng <flyflyflypeng@gmail.com>
2021-04-14 12:45:01 +0800

    add enum-3-3 example
    Signed-off-by: flyflypeng <flyflyflypeng@gmail.com>

---
+++ chapter-3/enum/enum-3-3/src/main.rs
@@ -0,0 +1,17 @@
+static LANGUAGE: &'static str = "Rust";
+const THRESHOLD: i32 = 10;
+
+fn is_big(n: i32) -> bool {
+    n > THRESHOLD
+}
+
+fn main() {
+    let n = 16;
+
+    println!("This is {}", LANGUAGE);
+    println!("The threshhold is {}", THRESHOLD);
+    println!("{} is {}", n, if is_big(n) { "big" } else { "small" });
+
+    // 报错！不能修改一个 `const` 常量。
+    // THRESHOLD = 5;
+}
